eddcbc10f2bb6584db266414c8fd43b40a66199f
ajrenaut <alexjrenaut@gmail.com>
2022-08-10 18:23:56 -0500

    Add initial login screen
    Adds rough draft login screen as well as assets for background and main menu
    icon.

--- Beanfinder/mainwindow.cpp
+++ Beanfinder/mainwindow.cpp
@@ -1,11 +1,21 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QPixmap backgroundMap("D:/Programming Projects/Beanfinder/Application/Beanfinder/assets/background");
+    QPixmap logoMap("D:/Programming Projects/Beanfinder/Application/Beanfinder/assets/logo");
+
+    const int backgroundWidth{ ui->background->width() };
+    const int backgroundHeight{ ui->background->height() };
+    ui->background->setPixmap( backgroundMap.scaled( backgroundWidth, backgroundHeight, Qt::KeepAspectRatio ) );
+
+    const int logoWidth{ ui->logo->width() };
+    const int logoHeight{ ui->logo->height() };
+    ui->logo->setPixmap( logoMap.scaled( logoWidth, logoHeight, Qt::KeepAspectRatio ) );
 }
 
 MainWindow::~MainWindow()
